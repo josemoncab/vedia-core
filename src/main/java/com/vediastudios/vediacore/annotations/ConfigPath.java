351d7bd9cdc91333680e3f3d03c1221c9a4451e5
Jose Montalvillo <josemoncab2001@gmail.com>
2025-04-24 18:43:02 +0200

    feat: Annotation based configurations

---
+++ src/main/java/com/vediastudios/vediacore/annotations/ConfigPath.java
@@ -0,0 +1,9 @@
+package com.vediastudios.vediacore.annotations;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ConfigPath {
+    String[] value();
+}
